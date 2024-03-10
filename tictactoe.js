9bbb27500f3fc61cd4906767bb08851f325b3360
robinm2812 <robinm2812@gmail.com>
2024-03-10 00:35:35 -0500

    Update tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,11 @@
-$(function(){
+$(function () {
     let board = initial_state();
     player();
     actions(board);
+    const X = "X";
+    const O = "O";
+    const xwin = [X, X, X];
+    const owin = [O, O, O];
 
     function player() {
         let counter = 0;
@@ -13,34 +17,39 @@ $(function(){
             }
         }
         if (counter == 0) return null;
-        else if (counter % 2 == 0) return "O";
-        else return "X";
+        else if (counter % 2 == 0) return O;
+        else return X;
     }
 
-    function actions(board){
+    function actions(board) {
         const action = new Set();
         for (let i = 0; i < board.length; i++) {
             const row = board[i];
             for (let j = 0; j < row.length; j++) {
                 const cell = row[j];
-                if(cell == null){
-                    action.add([i,j]);
+                if (cell == null) {
+                    action.add([i, j]);
                 }
             }
         }
         return action;
     }
 
-    function result(board,action){
-        let newBoard = $.extend(true,[],board);
+    function result(board, action) {
+        let newBoard = $.extend(true, [], board);
         let cplayer = player(newBoard);
         const i = action[0];
         const j = action[1];
-        if (newBoard[i][j] != null){
+        if (newBoard[i][j] != null) {
             throw new Error("Invalid Move");
         }
         newBoard[i][j] = cplayer;
         return newBoard;
+    }
+
+    function winner(board) {
+        let winlines = [];
+
     }
 
     function initial_state() {
