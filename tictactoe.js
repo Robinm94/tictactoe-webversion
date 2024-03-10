098c82fc2cdc14cdaedbec1ec0b06ce93471a4e3
robinm2812 <robinm2812@gmail.com>
2024-03-09 23:57:57 -0500

    Initial Commit

---
+++ tictactoe.js
@@ -0,0 +1,49 @@
+$(function(){
+    let board = initial_state();
+    player();
+    actions(board);
+
+    function player() {
+        let counter = 0;
+        for (const row of board) {
+            for (const cell of row) {
+                if (cell == null) {
+                    counter++;
+                }
+            }
+        }
+        if (counter == 0) return null;
+        else if (counter % 2 == 0) return "O";
+        else return "X";
+    }
+
+    function actions(board){
+        const action = new Set();
+        for (let i = 0; i < board.length; i++) {
+            const row = board[i];
+            for (let j = 0; j < row.length; j++) {
+                const cell = row[j];
+                if(cell == null){
+                    action.add([i,j]);
+                }
+            }
+        }
+        return action;
+    }
+
+    function result(board,action){
+        let newBoard = $.extend(true,[],board);
+        let cplayer = player(newBoard);
+        const i = action[0];
+        const j = action[1];
+        if (newBoard[i][j] != null){
+            throw new Error("Invalid Move");
+        }
+        newBoard[i][j] = cplayer;
+        return newBoard;
+    }
+
+    function initial_state() {
+        return [[null, null, null], [null, null, null], [null, null, null]];
+    }
+});
